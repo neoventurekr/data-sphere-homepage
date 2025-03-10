70a245e4fee049b1ebc4b3025547f78d79be0c55
chaeney <chaekey@gmail.com>
2025-03-10 09:50:16 +0900

    first commit

---
+++ next.config.ts
@@ -0,0 +1,12 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  /* config options here */
+  reactStrictMode: true,
+  compiler: {
+    emotion: true, // SWC에서 Emotion 지원 활성화
+  },
+  output: "export"
+};
+
+export default nextConfig;
---
+++ src/pages/index.tsx
@@ -0,0 +1,15 @@
+import styled from '@emotion/styled';
+
+const Wrapper = styled.div`
+    max-width: 1920px;
+    height: 100%;
+    margin: 0 auto;
+`
+
+export default function Home() {
+
+    
+  return (
+    <div></div>
+  );
+}
