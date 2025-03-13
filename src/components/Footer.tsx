6416c653389d382b9cf73c5817fc7a79f50bd4df
chaeney <chaekey@gmail.com>
2025-03-13 10:36:26 +0900

    footer add link

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,12 +3,13 @@ import {useCustomTranslation} from "@/hook/useCustomTranslation";
 
 const Wrapper = styled.footer`
     display: flex;
+    flex-wrap: wrap;
     justify-content: space-between;
     align-items: flex-start;
     max-width: 1280px;
     width: 100%;
     margin: 0 auto;
-    padding: 120px 0;
+    padding: 120px 0 52px;
     background: var(--white, #FFF);
     
     .logo {
@@ -25,18 +26,27 @@ const Wrapper = styled.footer`
         display: flex;
         flex-direction: column;
         align-items: flex-start;
-        gap: 20px;
+        gap: 14px;
         
         p {
             color: #787878;
             font-size: 24px;
         }
         
-        button {
+        a {
             color: #9F9F9F;
             font-size: 20px;
             font-weight: 400;
         }
+    }
+    
+    .copyright {
+        width: 100%;
+        margin-top: 80px;
+        flex-shrink: 0;
+        color: #CACACA;
+        font-size: 18px;
+        text-align: center;
     }
 `
 
@@ -51,32 +61,34 @@ const Footer = () => {
       <div className={'link-container'}>
         <div className={'link-group'}>
           <p>{t('footer.security')}</p>
-          <button>{t('footer.security_privacy')}</button>
-          <button>{t('footer.security_policy')}</button>
-          <button>{t('footer.security_termOfUse')}</button>
-          <button>{t('footer.security_legalNotice')}</button>
+          <a href={'#'} target={'_blank'} rel={'noopener noreferrer'}>{t('footer.security_privacy')}</a>
+          <a href={'#'} target={'_blank'} rel={'noopener noreferrer'}>{t('footer.security_policy')}</a>
+          <a href={'#'} target={'_blank'} rel={'noopener noreferrer'}>{t('footer.security_termOfUse')}</a>
+          <a href={'#'} target={'_blank'} rel={'noopener noreferrer'}>{t('footer.security_legalNotice')}</a>
         </div>
         
         <div className={'link-group'}>
           <p>{t('footer.explore')}</p>
-          <button>{t('footer.explore_docs')}</button>
-          <button>{t('footer.explore_medium')}</button>
-          <button>{t('footer.explore_github')}</button>
+          <a href={'#'} target={'_blank'} rel={'noopener noreferrer'} aria-disabled={true}>{t('footer.explore_docs')}</a>
+          <a href={'https://medium.com/@DataSphere_Official'} target={'_blank'} rel={'noopener noreferrer'}>{t('footer.explore_medium')}</a>
+          <a href={'#'} target={'_blank'} rel={'noopener noreferrer'}>{t('footer.explore_github')}</a>
         </div>
         
         <div className={'link-group'}>
           <p>{t('footer.community')}</p>
-          <button>{t('footer.community_twitter')}</button>
-          <button>{t('footer.community_telegram')}</button>
-          <button>{t('footer.community_youtube')}</button>
+          <a href={'https://x.com/DataSphere_data'} target={'_blank'} rel={'noopener noreferrer'}>{t('footer.community_twitter')}</a>
+          <a href={'https://t.me/datasphere_global'} target={'_blank'} rel={'noopener noreferrer'}>{t('footer.community_telegram')}</a>
+          <a href={'https://www.youtube.com/@DataSphere-data'} target={'_blank'} rel={'noopener noreferrer'}>{t('footer.community_youtube')}</a>
         </div>
         
         <div className={'link-group'}>
           <p>{t('footer.contactUs')}</p>
-          <button>{t('footer.contactUs_support')}</button>
-          <button>{t('footer.contactUs_email')}</button>
+          <a href={'mailto:support@deepdatasphere.com'} target={'_blank'} rel={'noopener noreferrer'}>{t('footer.contactUs_support')}</a>
+          <a href={'mailto:support@deepdatasphere.com'} target={'_blank'} rel={'noopener noreferrer'}>{t('footer.contactUs_email')}</a>
         </div>
       </div>
+      
+      <div className={'copyright'}>Copyright© Data Sphere. All rights reserved.</div>
     </Wrapper>
   );
 };
